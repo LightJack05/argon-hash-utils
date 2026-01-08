1f3de92be7ba227c11265885d5aed85be12cc781
LightJack05 <66321084+LightJack05@users.noreply.github.com>
2026-01-08 16:00:14 +0100

    Add lib

---
+++ hashStruct.go
@@ -0,0 +1,11 @@
+package argonhashutils
+
+// Argon2idPasswordHash represents an Argon2id hashed password with its parameters
+type Argon2idPasswordHash struct {
+	Version     int
+	Salt        []byte
+	Hash        []byte
+	Time        uint32
+	Memory      uint32
+	Parallelism uint8
+}
